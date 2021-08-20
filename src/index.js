915027d3734498423ea67f2b5084de56a6b52328
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-20 18:23:40 -0300

    Retomar da secao 2.9 da apostila 06, sobre estado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+
+class App extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            latitude: null,
+            longitude: null,
+            estacao: null,
+            data: null,
+            icone: null
+        }
+    }
+
+    obterEstacao = (data, latitude) => {
+        const anoAtual = data.getFullYear()
+        const d1 = new Date(anoAtual, 5, 21)
+        const d2 = new Date (anoAtual, 8, 24)
+        const d3 = new Date (anoAtual, 11, 22)
+        const d4 = new Date (anoAtual, 2, 21)
+        const sul = latitude < 0;
+        if (data >= d1 && data < d2)
+            return sul ? 'Inverno' : 'Verão'
+        if (data >= d2 && data < d3)
+            return sul ? 'Primavera' : 'Outono'
+        if (data >= d3 && data < d4)
+            return sul ? 'Verão' : 'Inverno'
+        return sul ? 'Outono' : 'Primavera'
+    }
+
+    icones = {
+        'Primavera': 'fa-seedling',
+        'Verão': 'fa-umbrella-beach',
+        'Outono': 'fa-tree',
+        'Inverno': 'fa-snowman'
+    }
+
+    obterLocalizacao = () => {
+        window.navigator.geolocation.getCurrentPosition(
+            (posicao) => {
+                let data = new Date()
+                let estacao = this.obterEstacao(data, posicao.coords.latitude)
+                let icone = this.icones[estacao]
+                this.setState({
+                    latitude: posicao.coords.latitude,
+                    longitude: posicao.coords.longitude,
+                    estacao: estacao,
+                    data: data.toLocaleTimeString(),
+                    icone: icone       
+                })
+            }
+        )
+    }
+
+
+    render(){
+        this.obterLocalizacao()
+        return (
+            <div>
+                {JSON.stringify(this.state)}
+            </div>
+        )   
+    }
+}
+
+
+// export default function App() {
+
+//     window.navigator.geolocation.getCurrentPosition(
+//         (posicao) => console.log(posicao)
+//     )
+//     return (
+//         <div>
+//             Meu app
+//         </div>
+//     )
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
